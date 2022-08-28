2a685d6b1673fc24ac0bf6c355eff01196c2d6dc
Jan Rolenc <jrolenc@centrum.cz>
2022-08-28 12:46:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import skull from "./photos/skull.jpg"
+import { useState } from 'react'
+
 
 function App() {
+  const [largerImage, setLargerImage] = useState(false)
+
+  const extendImage = () => {
+    setLargerImage(!largerImage)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="app">
+    !largerImage ? (
+      <div className="app-sub">
+        <div className="app-sub__left">
+          <h2>Jan Rolenc</h2>
+          <h2>About me</h2>
+          <h2>Contact</h2>
+        </div>
+        <div className="app-sub__right">
+          <h1>My Portfolio</h1>
+          <div className="app-sub__right__photo">
+            <img src={skull} alt="skull" onClick={extendImage} />
+            <h2>Skull</h2>
+          </div>
+        </div>
+      </div>
+    ) :
+      (
+        <div className="app-sub2">
+          <div className='largerImg'>
+            <img src={skull} alt="skull" onClick={extendImage} />
+          </div>
+
+        </div>
+
+      )
+    // </div>
   );
 }
 
